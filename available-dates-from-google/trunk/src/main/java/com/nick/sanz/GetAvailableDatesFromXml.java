36f9c48e6ec7b1886eb6d264c2598d3418e3baad
nsanzeri <nsanzeri@de15ca61-e718-418b-a806-fd98189dfc8b>
2014-02-27 21:51:22 +0000

    Adding Escape Utils

--- available-dates-from-google/trunk/src/main/java/com/nick/sanz/GetAvailableDatesFromXml.java
+++ available-dates-from-google/trunk/src/main/java/com/nick/sanz/GetAvailableDatesFromXml.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
