744042300e570291b4a661c841fd217ba2f72b4b
nsanzeri <nsanzeri@10.0.0.3>
2015-02-26 19:10:20 -0600

    asdf

--- available-dates-from-google/trunk/src/main/java/com/nick/sanz/TodayInHistory.java
+++ available-dates-from-google/trunk/src/main/java/com/nick/sanz/TodayInHistory.java
@@ -26,41 +26,25 @@ public class TodayInHistory {
 		List<String> datesTaken = new ArrayList<String>();
 		List<SyndEntry> entries = sf.getEntries();
 		StringBuffer sb = new StringBuffer();
-		sb.append("Today in Hooked On Sonics history: ");
 		Boolean firstDate = true;
 		String holdYear = "2002";
 		System.out.println("\n==== Year = " + holdYear);
-		for (SyndEntry entry : entries) {
-			SyndContent description = entry.getDescription();
-			String title = StringEscapeUtils.unescapeHtml(entry.getTitle());
-			String where = description.getValue().substring((description.getValue().indexOf("Where:") + 7), (description.getValue().indexOf("<br>Event") - 1));
-			String date = description.getValue().substring(10, (description.getValue().indexOf(",") + 6));
-			String monthday = description.getValue().substring(10, (description.getValue().indexOf(",")));
-			String year = description.getValue().substring(description.getValue().indexOf(",") + 2, description.getValue().indexOf(",") + 6);
-			if (!year.equals(holdYear)){
-				System.out.println("\n==== Year = " + year);
-				holdYear = year;
-			}
-			System.out.println(StringEscapeUtils.unescapeHtml(entry.getTitle()));
-			String month = monthenum.make(cal.get(Calendar.MONTH) + 1).toShortString();
-			if (date.contains(" " + (cal.get(Calendar.DATE) + 0) + ",")
-					&& date.contains(month)
-					){
-				if (firstDate){
-					sb.append(monthday + "\n\n");	
-				}else{
-					sb.append( "\n");
-				}
-				firstDate = false;
-				sb.append(year + " - " + title + " (" + where + ")\n");
-				
-			}
-		}
-		sb.append("#HookedOnSonicsHistory\n\n");
 		System.out.println("============= Historic events ==========================");
-		System.out.println(sb.toString());
+		int dayCount = 6;
+		int counter = dayCount;
+		boolean firstRun = true;
+		for (int i = 0; i < counter; i++) {
+			sb.append("Today in Hooked On Sonics history: ");
+			createEntry(monthenum, cal, entries, sb, firstDate, holdYear, dayCount, firstRun);
+			sb.append("\n");
+			dayCount--;
+			firstRun = false;
+		}
+		System.out.println("#HookedOnSonicsHistory\n");
 		System.out.println("============= Historic events - END ==========================");
-
+			
+//		Hooked On Sonics @ 
+//		HOS @ 
 		//		System.out.println("============= Historic events ==========================");
 //		for (SyndEntry entry : entries) {
 //			SyndContent description = entry.getDescription();
@@ -68,6 +52,37 @@ public class TodayInHistory {
 //			datesTaken.add(description.getValue().substring(6, description.getValue().indexOf(",")));
 //		}
 
+	}
+
+	private static void createEntry(Month monthenum, Calendar cal, List<SyndEntry> entries, StringBuffer sb, Boolean firstDate, String holdYear, int dayCount, boolean firstRun) {
+		for (SyndEntry entry : entries) {
+			SyndContent description = entry.getDescription();
+			String title = StringEscapeUtils.unescapeHtml(entry.getTitle());
+			String where = description.getValue().substring((description.getValue().indexOf("Where:") + 7), (description.getValue().indexOf("<br>Event") - 1));
+			String date = description.getValue().substring(10, (description.getValue().indexOf(",") + 6));
+			String monthday = description.getValue().substring(10, (description.getValue().indexOf(",")));
+			String year = description.getValue().substring(description.getValue().indexOf(",") + 2, description.getValue().indexOf(",") + 6);
+			if (firstRun) {
+				if (!year.equals(holdYear)) {
+					System.out.println("\n==== Year = " + year);
+					holdYear = year;
+				}
+				System.out.println(StringEscapeUtils.unescapeHtml(entry.getTitle()) + "   ---  " + date);
+			}
+			String month = monthenum.make(cal.get(Calendar.MONTH) + 1).toShortString();
+			if (date.contains(" " + (cal.get(Calendar.DATE) - (dayCount - 1)) + ",") && date.contains(month)) {
+				if (firstDate) {
+					sb.append(monthday + "\n");
+				}
+				firstDate = false;
+				sb.append(year + " - " + title + " (" + where + ")\n");
+				
+			}
+		}
+		
+		
+		System.out.println(sb.toString());
+		
 	}
 	
 	public enum Month {
